e2d1aab938eea9b6f42d878c9835478915e5956c
Amer-Alkharashgah <buildhorizon@gmail.com>
2019-10-19 01:18:31 +0300

    Auth with navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
-import React from 'react';
+// import React from 'react';
+// import List from './List';
+
+
+// function App() {
+//   return (
+//     <div className="container">
+//       <div className="row">
+//         <div className="col-md-6 mx-auto">
+//           <h1 className="text-center">Todo list App</h1>
+//           <List/>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
+import Login from './components/Login'
+import Register from './components/Register'
+import Profile from './components/Profile'
 import List from './List'
 
-
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6 mx-auto">
-          <h1 className="text-center">Todo list App</h1>
-          <List/>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/List" component={List} />
+          </div>
         </div>
-      </div>
-    </div>
-  );
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/ListCrud.js
+++ src/ListCrud.js
@@ -1,19 +1,39 @@
 import axios from "axios"
+import { getProfile } from './components/UserFunctions'
+
+let user_id="";
 
 export const getList = () => {  // get all tasks 
+    getProfile().then(res => {
+     
+            user_id= res.user.id;
+            console.log(user_id)
+    
+    })
     return axios
     .get('/api/tasks',{
         headers : {'content-Type':'application/json'}
     })
     .then(res =>{
-        return res.data
+        
+        let filtered_tasks=[];
+
+         for(let i=0;i<res.data.length;i++){
+
+            if (user_id===res.data[i].user_id){
+                filtered_tasks.push(res.data[i])
+            }
+        }
+        return filtered_tasks;
     })
 }
 
 export const addItem = title => { //add task 
+    console.log(user_id)
     return axios
     .post('/api/tasks',{
-        title:title
+        title:title,
+        user_id:user_id
     },
     {
         headers:{'content-Type':'application/json'}
